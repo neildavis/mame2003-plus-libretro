b95d3f14f9134a9964e9a88ef9f71a943f34766a
Neil Davis <neildavis@users.noreply.github.com>
2023-02-08 21:20:58 +0000

    Replace C++ style comments

--- src/output.c
+++ src/output.c
@@ -39,13 +39,13 @@ void output_init(const char *machine_name) {
 	if (fd_outputs < 0) {
 		fd_outputs = open(OUTPUTS_PIPE_NAME, O_WRONLY | O_NONBLOCK);
 		if (fd_outputs > -1) {
-			// Successfully opened pipe. Now get a FILE stream
+			/* Successfully opened pipe. Now get a FILE stream */
 			stream_outputs = fdopen(fd_outputs, "w");
 			if (NULL == stream_outputs) {
 				close(fd_outputs);
 				fd_outputs = -1;
 			} else {
-				//Send a special 'hello' output to allow server to initialize for specific ROM
+				/* Send a special 'hello' output to allow server to initialize for specific ROM */
 				output_set_value(OUTPUTS_INIT_NAME, 0);
 			}
 		}
@@ -103,7 +103,7 @@ void output_set_indexed_value(const char *basename, int index, int value) {
 	if (index >= 100) *dest++ = '0' + ((index / 100) % 10);
 	if (index >= 10) *dest++ = '0' + ((index / 10) % 10);
 	*dest++ = '0' + (index % 10);
-	*dest++ = 0;	// null terminator
+	*dest++ = 0;	/* null terminator */
 
 	/* set the value */
 	output_set_value(buffer, value);
