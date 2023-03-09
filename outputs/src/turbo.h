67ab3cd4ea54c023b0c4ee719eabee35e87517df
Neil Davis <neildavis@users.noreply.github.com>
2023-03-09 21:39:22 +0000

    Turbo: outputs: Squashed commits
    remove unused code & consts (+9 squashed commits)
    [c836b368] Turbo: outputs: replace cars passed pie with arc
    [c98dd9ff] Turbo: outputs draw time arc & cp over logo img bg
    [1df8b33c] Turbo: outputs lcd bg color change
    [1c199a13] Turbo: outputs shows logo during start screen
    [ad085a73] Turbo: outputs: show  'round' & 'lives' on tm1637
    [ea72a854] Turbo: outputs draw cars passed pie in yellow under yellow flags.
    [de3523a4] Turbo outputs: Add cars passed pie slice on ST7789
    [0b2f6732] Turbo outputs: Draw time on ST7789
    [223cc2f2] Turbo: say 'press start button' on TM1637 on start screen

--- outputs/src/turbo.h
+++ outputs/src/turbo.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <tm1637.h>
 #include <tm1637_sayer.h>
+#include <udd.h>
 
 #include "output_handler_base.h"
 
@@ -11,6 +12,7 @@ using namespace tm1637;
 
 class TurboOutputHandler : public MOutputHandler {
 public:
+    TurboOutputHandler();
     // MOutputHandler overrides
     virtual ~TurboOutputHandler() override;
     virtual void init() override;
@@ -24,12 +26,26 @@ private:
     void update_start_button(int value);
     void update_attract_mode(int value);
     void update_start_mode(int value);
+    void update_lives(int value);
+    void update_stage(int value);
+    void update_tm1637();
     void reset_state();
+    void draw_time_arc();
+    void draw_cars_passed_arc();
 private:
     std::shared_ptr<Device> m_pTM1637;
     std::unique_ptr<Sayer> m_pSayer;
-    int m_tm1637_digits[4];
+    udd::DisplayST7789R m_display;
+    udd::Image m_image;
+    udd::Image m_logoImage;
+    uint8_t m_tm1637_digits[4];
     int m_start_lights_last;
+    int m_time;
+    int m_time_max;
+    int m_cars_passed;
+    int m_last_yellow_flag;
+    int m_lives;
+    int m_stage;
     bool m_attract_mode_active;
     bool m_start_mode_active;
 };
