89958176c7a89b67d6a066290a55392c1bf4764f
Neil Davis <3934226+neildavis@users.noreply.github.com>
2025-12-13 12:06:45 +0000

    Outputs: server cleanup on OS signals

--- outputs/src/main.cpp
+++ outputs/src/main.cpp
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/timeb.h>
+#include <signal.h>
 
 #include "utils.h"
 #include "output-def.h"
@@ -19,21 +20,31 @@
 #include "monacogp.h"
 #include "chasehq.h"
 
+
+static volatile sig_atomic_t keep_running = 1;
+
+static void sig_handler(int _)
+{
+    (void)_;
+    keep_running = 0;
+}
+
 void main_event_loop() {
-    int fd;
+    FILE *stream = NULL;
+    int fd  = -1;
     char machine_name[OUTPUTS_PIPE_MAX_MACHINE_NAME_SIZE];
     char output_name[OUTPUTS_PIPE_MAX_OUTPUT_NAME_SIZE];
     int output_value;
     
-    while (1) {
+    while (keep_running) {
         /* Open the FIFO pipe for reading */
         fprintf(stdout, "%s: Opening FIFO pipe at %s\n", proc_name, OUTPUTS_PIPE_NAME);
-        fd = open(OUTPUTS_PIPE_NAME, O_RDONLY);
+        fd = open(OUTPUTS_PIPE_NAME, O_RDONLY|O_NONBLOCK);
         if (fd < 0) {
             fprintf(stderr, "%s: Unable to open FIFO pipe for reading at %s errno=%d\n", proc_name, OUTPUTS_PIPE_NAME, errno);
             exit(errno);
         }
-        FILE *stream = fdopen(fd, "r");
+        stream = fdopen(fd, "r");
         if (NULL == stream) {
             fprintf(stderr, "%s: Unable to open FIFO pipe stream for reading. errno=%d\n", proc_name, errno);
             exit(errno);
@@ -45,9 +56,22 @@ void main_event_loop() {
         pfd.fd = fd;
         pfd.events = POLLIN;
         pfd.revents = 0;
+        int poll_ret = 0;
         std::unique_ptr<MOutputHandler> pOutputHandler;
-        while (1) {
-            poll(&pfd, 1, -1);
+        while (poll_ret >= 0 && keep_running) {
+            poll_ret = poll(&pfd, 1, -1);
+            if (poll_ret < 0) {
+                /* poll failed */
+                fprintf(stdout, "%s: Poll failed, errno=%d", proc_name, errno);
+                if (EINTR == errno) {
+                    /* Interrupted by signal */
+                    fprintf(stdout, " (Poll interrupted by signal)\n");
+                } else {
+                    /* Other unspecified failure */
+                    fprintf(stdout, "\n");
+                }
+                break;
+            }
             if (pfd.revents & POLLHUP) {
                 /* client closed their end of the pipe */
                 fprintf(stdout, "%s: Client hung-up pipe\n", proc_name);
@@ -114,14 +138,28 @@ void main_event_loop() {
         /* Close the pipe ready to re-open for next client */
         fprintf(stdout, "%s: Closing stream\n", proc_name);
         fclose(stream);
+        stream = NULL;
         fprintf(stdout, "%s: Closing pipe\n", proc_name);
         close(fd);
+        fd = -1;
+    }
+    // Clean up on exit
+    fprintf(stdout, "%s: Exiting main event loop, cleaning up\n", proc_name);
+    if (NULL != stream) {
+        fclose(stream);
+    }
+    if (fd >= 0) {
+        close(fd); 
     }
 }
 
 int main(int /*argc*/, char **argv) {
     int err;
     proc_name = argv[0];
+
+    /* Setup signal handlers */
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
 
     /* Make the FIFO pipe */
     fprintf(stdout, "%s: Creating FIFO pipe at %s\n", argv[0], OUTPUTS_PIPE_NAME);
@@ -131,6 +169,13 @@ int main(int /*argc*/, char **argv) {
         exit(errno);
     }
 
-    /* Run the main event loop forever */
+    /* Run the main event loop */
     main_event_loop();
+
+    /* Cleanup FIFO */
+    err = unlink(OUTPUTS_PIPE_NAME);
+    if (err != 0) {
+        fprintf(stderr, "%s: Unable to remove FIFO pipe at %s errno=%d\n", argv[0], OUTPUTS_PIPE_NAME, errno);
+        exit(errno);
+    }
 }
